09cdbfb4ac2df0854aac9c351665fa1c45f4c643
Alexander Peters <tutonga@gmail.com>
2021-10-21 15:55:55 +0200

    first haskell

---
+++ 8kyu-reversed-strings.hs
@@ -0,0 +1,12 @@
+{-
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+-}
+
+module Solution where
+
+solution :: String -> String
+solution = reverse
